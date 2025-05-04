072009a79388f97b5d083948532a883371e81f2e
Yixian17 <wongyixian17@gmail.com>
2025-05-04 22:11:38 +0800

    Update RecipeDTO.java

--- olive-backend/src/main/java/com/olive_backend/Recipe/Vault/Recipe/RecipeDTO.java
+++ olive-backend/src/main/java/com/olive_backend/Recipe/Vault/Recipe/RecipeDTO.java
@@ -16,43 +16,85 @@ public class RecipeDTO {
     private LocalDateTime createdDate;
     private List<IngredientDTO> ingredients;
 
-    // Constructors
-    public RecipeDTO() {}
+    // Constructor
+    public RecipeDTO() {
+    }
 
-    public RecipeDTO(Long id, String title, String instructions,Difficulty difficulty, String creatorName, LocalDateTime createdDate,  String imageUrl, List<IngredientDTO> ingredients) {
+    public RecipeDTO(Long id, String title, String instructions, Difficulty difficulty, String creatorName,
+            LocalDateTime createdDate, String imageUrl, List<IngredientDTO> ingredients) {
         this.id = id;
         this.title = title;
         this.instructions = instructions;
         this.difficulty = difficulty;
         this.creatorName = creatorName;
-        this .createdDate = createdDate;
+        this.createdDate = createdDate;
         this.imageUrl = imageUrl;
         this.ingredients = ingredients;
     }
 
     // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String name) { this.title = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getImageUrl() { return imageUrl; }
-    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+    public String getTitle() {
+        return title;
+    }
 
-    public String getInstruction() { return instructions; }
-    public void setInstructions(String description) { this.instructions = instructions; }
+    public void setTitle(String name) {
+        this.title = name;
+    }
 
-    public List<IngredientDTO> getIngredients() { return ingredients; }
-    public void setIngredients(List<IngredientDTO> ingredients) { this.ingredients = ingredients; }
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
-    public Difficulty getDifficulty() { return difficulty; }
-    public void setDifficulty(Difficulty difficulty) { this.difficulty = difficulty; }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 
-    public String getCreatorName() { return creatorName; }
-    public void setCreatorName(String creatorName) { this.creatorName = creatorName;}
+    public String getInstruction() {
+        return instructions;
+    }
 
-    public LocalDateTime getCreatedDate() { return createdDate; }
-    public void setCreatedDate(LocalDateTime createdDate) { this.createdDate = createdDate;}
+    public void setInstructions(String description) {
+        this.instructions = instructions;
+    }
+
+    public List<IngredientDTO> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<IngredientDTO> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
 
 }
